eee4d1b5da8f19d1c3d4575ba5d9040e3fe8cf99
vovBorya <vovbor02@gmail.com>
2021-01-14 22:34:22 +0200

    Refactored app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -9,14 +9,11 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 type Props = {};
 
-const App: React.FC<Props> = ({}) => {
-
-  return (
-    <Router>
-      <Navbar/>
-      <Route path='/works' component={WorksPage}/>
-    </Router>
-  );
-};
+const App: React.FC = ({}: Props) => (
+  <Router>
+    <Navbar/>
+    <Route path='/works' component={WorksPage}/>
+  </Router>
+);
 
 export default App;
